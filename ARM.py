071c2df1e45ce5666307ea0d9ea1361fc9c5a01b
ck970 <117049206+ck970@users.noreply.github.com>
2023-03-28 15:36:57 -0400

    Update ARM.py

--- ARM.py
+++ ARM.py
@@ -6,64 +6,101 @@ import itertools
 import time
 
 def load_file():
-
     data, meta = loadarff('basket.arff')
     df = pd.DataFrame(data)
     i = 0
     for dtype in df.dtypes:
         df[df.columns[i]] = df[df.columns[i]].str.decode("utf-8")
         i += 1
-
     return df
 
-def transform_df_arr_hash(df):
-    '''
-    :param df: your df
-    :return: the df expressed as an array of hash maps or technically in python list of dicts
-    '''
-    arr_hash = df.to_dict('records')
+# -----------------------------
 
+def transform_df_arr_hash(df):
+    arr_hash = df.to_dict('records')
     return arr_hash
 
+# -----------------------------
+
 def transform_dict_to_set(dict_vals_0_1):
-    '''
-    :param dict_vals_0_1: dictionary where the values are 0 and 1 corresponding to if key is present
-    :return: set of only those cols that are 'present'
-    '''
     return set(key for key, val in dict_vals_0_1.items() if val == '1')
 
+# -----------------------------
+
 def transform_ah_to_arr_sets(arr_hash):
-    '''
-    :param arr_hash: the list of dicts returned from transform_df_arr_hash
-    :return: a list of sets where each set only contains those columns present in transaction row represents
-    '''
     return [transform_dict_to_set(dict_0_1) for dict_0_1 in arr_hash]
 
-def a_subset_b(set_a, set_b):
-    '''
-    :param set_a: the set that we want to see if it is a subset of set b
-    :param set_b: see above
-    :return: true if set_a is a subset of set_b, false otherwise
-    '''
+# -----------------------------
 
-    # issubset can either be performed via explicit call or <= shorthand
-    temp_1 = set_a.issubset(set_b)
-    temp_2 = (set_a <= set_b)
+def get_support(arr_sets, item):
+    count = 0
+    for row in arr_sets:
+        if item.issubset(row):
+            count += 1
+    return count
 
-    return temp_1
+# -----------------------------
 
+def get_confidence(arr_sets, item_a, item_b):
+    count = 0
+    for row in arr_sets:
+        if item_a.issubset(row) and item_b.issubset(row):
+            count += 1
+    return count / get_support(arr_sets, item_a)
+
+# -----------------------------
+
+def get_lift(arr_sets, item_a, item_b):
+    return get_confidence(arr_sets, item_a, item_b) / get_support(arr_sets, item_b)
+
+# -----------------------------
+
+def get_candidates(items, k):
+    candidates = set()
+    for item_a in items:
+        for item_b in items:
+            if len(item_a.union(item_b)) == k:
+                candidates.add(item_a.union(item_b))
+    return candidates
+
+# -----------------------------
+
+def apriori(arr_sets, min_support):
+    items = set()
+    for row in arr_sets:
+        for item in row:
+            items.add(frozenset([item]))
+    k = 2
+    while len(items) > 0:
+        items = get_candidates(items, k)
+        items = set([item for item in items if get_support(arr_sets, item) >= min_support])
+        k += 1
+    return items
+
+# -----------------------------
+
+def get_association_rules(arr_sets, min_confidence, min_lift, min_support):
+    items = apriori(arr_sets, min_support)
+    rules = []
+    for item in items:
+        for i in range(1, len(item)):
+            for subset in itertools.combinations(item, i):
+                subset = frozenset(subset)
+                if get_confidence(arr_sets, subset, item - subset) >= min_confidence and \
+                        get_lift(arr_sets, subset, item - subset) >= min_lift and \
+                        get_support(arr_sets, subset) >= min_support:
+                    rules.append((subset, item - subset))
+    return rules
+# -----------------------------
 
 def main():
     df = load_file()
     arr_hash = transform_df_arr_hash(df)
     arr_sets = transform_ah_to_arr_sets(arr_hash)
-    print(arr_sets[0])
+    rules = get_association_rules(arr_sets, 0.5, 1, 10)
+    print(rules)
 
-    # Found out how to get the names of columns that are present in a given transaction, now need to order them
-
-    # print(a_subset_b({1,2}, {1,2,3}))
-    # print(a_subset_b({1, 4}, {1, 2, 3}))
-
+# -----------------------------
 
 if __name__ == '__main__':
     main()
